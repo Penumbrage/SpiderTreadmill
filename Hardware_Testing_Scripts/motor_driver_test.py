97732775d144f1501215331a200b8c12ec7c854e
Penumbrage <wwangVer2.0@gmail.com>
2022-05-28 04:17:33 -0600

    added comments to the motor driver test code. Also testing TODO bot

--- Hardware_Testing_Scripts/motor_driver_test.py
+++ Hardware_Testing_Scripts/motor_driver_test.py
@@ -7,9 +7,16 @@ import time
 import board
 from adafruit_motorkit import MotorKit
 
-# 
+#----- Code for the DC motor ---------#
+# create driver object
 kit = MotorKit(i2c=board.I2C())
 
+# run motor at full speed for 0.5 seconds
 kit.motor1.throttle = 1.0
 time.sleep(0.5)
+
+# stop the motor
 kit.motor1.throttle = 0
+#-------------------------------------#
+
+# TODO: Include the stepper motor test code 
