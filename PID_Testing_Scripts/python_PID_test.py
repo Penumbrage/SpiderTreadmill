5726764ad56d0dc147e0a9613b25e7706190693d
Penumbrage <wwangVer2.0@gmail.com>
2022-06-26 01:56:07 -0600

    created code that tests the ramping idea for motor control

--- PID_Testing_Scripts/python_PID_test.py
+++ PID_Testing_Scripts/python_PID_test.py
@@ -107,6 +107,46 @@ def readEncoder(channel):
 
     pos_i = pos_i + increment
 
+# function used to change the motor velocity based on the user input and current velocity
+def changeMotorVelocity():
+    global speed_des
+
+    # NOTE: built into this function is the ability to "ramp" from the current velocity
+    #       to the desired velocity to provide smoother transitions between velocities
+    #       that also minimize strain of sharp fluctuations of speed on the motor and
+    #       the motor driver
+
+    # number of loop iterations for the ramp signal
+    iter = 10
+
+    # obtain the current velocity from the motor
+    m_vel = calcMotorVelocity()
+
+    # create a variable to store the ramp speed in the loop (starts at the current speed)
+    ramp_vel = m_vel
+
+    # determine the increment for the ramp for a loop of 10 iterations
+    signal_increment = (speed_des - m_vel)/iter
+
+    # ramp the function from the current m_vel to the desired velocity via a for loop
+    for i in range(iter):
+        # obtain the ramp velocities
+        ramp_vel = ramp_vel + signal_increment
+
+        # use PID function to determine speed to be sent to controller and set that speed
+        control_sig = motorPID(ramp_vel, m_vel)
+
+        # send the motor speed
+        motor1.setSpeed(control_sig)
+
+        # wait for the motor to react to the control signal
+        time.sleep(0.1)
+
+        # reobtain the current speed of the motor for the next iteration of the loop
+        m_vel = calcMotorVelocity()
+
+    return control_sig, m_vel           # return final control signal and motor velocity
+
 # Create interrupt to ENCA
 GPIO.add_event_detect(ENCA, GPIO.RISING, callback = readEncoder)
 
@@ -132,6 +172,18 @@ def getUserInput(q):
             print("You did not enter a number in the correct format (check for any unwanted characters, spaces, etc).")
             print("Please enter your speed again.")
 
+# Create a function that reads the user input from the queue in the main thread and updates the speed_des variable
+def readUserInput():
+    global speed_des, q
+
+    # determine the desired speed from the user (in m/s) and covert it to RPM
+    try:
+        speed_des = q.get(block=False)                  # False used to prevent code on waiting for info
+        speed_des = speed_des*(60/pi)/(2/39.3701)       # conversion to RPM
+
+    except queue.Empty:
+        pass
+
 # Define a custom exception to raise if a fault is detected.
 class DriverFault(Exception):
     def __init__(self, driver_num):
@@ -143,9 +195,6 @@ def raiseIfFault():
 
 # Main execution loop for the motor
 if __name__ == '__main__':
-
-     # set counter that dictates how often information is printed to the terminal
-    print_counter = 0
 
     # create a thread that runs the user input function (obtain user defined speeds)
     # NOTE: This is a daemon thread which allows the thread to be killed when the main program is killed,
@@ -160,31 +209,14 @@ if __name__ == '__main__':
             # test for driver faults
             raiseIfFault()
 
-            # determine the desired speed from the user (in m/s) and covert it to RPM
-            try:
-                speed_des = q.get(block=False)               # False used to prevent code on waiting for info
-                speed_des = speed_des*(60/pi)/(2/39.3701)    # conversion to RPM
+            # obtain user input for desired velocity if available from the queue
+            readUserInput()
 
-            except queue.Empty:
-                pass
-
-            # determine the motor velocity from encoder
-            m_vel = calcMotorVelocity()
-
-            # use PID function to determine speed to be sent to controller and set that speed
-            control_sig = motorPID(speed_des, m_vel)
-
-            # send the motor speed
-            motor1.setSpeed(control_sig)
+            # set the motor speed determined from user input and current motor speeds (ramping included)
+            control_sig, m_vel = changeMotorVelocity()
 
             # print useful information about motor speeds
-            if (print_counter % 10 == 0):
-                print(control_sig, "|", speed_des, "|", m_vel)
-
-            print_counter = print_counter + 1
-
-            # pause the loop to ensure we are not aliasing the encoder readings
-            time.sleep(0.1)
+            print(control_sig, "|", speed_des, "|", m_vel)
 
     except KeyboardInterrupt:
         print("\nKeyboard Interrupt")
